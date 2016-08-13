7fd04e024920164014da4396e5cab31090e0bdd3
shtakai <shtakai@gmail.com>
2016-08-13 21:22:13 +0900

    added db, session

--- conftest.py
+++ conftest.py
@@ -1,8 +1,38 @@
 import pytest
-from application import create_app
+import os
+from application import create_app, db as database
+
+DB_LOCATION = '/tmp/test_app.db'
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def app():
     app = create_app()
     return app
+
+
+@pytest.fixture(scope='session')
+def db(app, request):
+    if os.path.exists(DB_LOCATION):
+        os.unlink(DB_LOCATION)
+
+    database.app = app
+    database.create_all
+
+    def teardown():
+        database.drop_all()
+        os.unlink(DB_LOCATION)
+    request.addfinalizer(teardown)
+    return database
+
+
+@pytest.fixture(scope='function')
+def session(db, request):
+    session = db.create_scoped_session()
+    db.session = session
+
+    def teardown():
+        session.remove()
+
+    request.addfinalizer(teardown)
+    return session
