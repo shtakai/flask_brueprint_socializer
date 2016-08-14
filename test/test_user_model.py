a0f07d614127dcbd740a80b09946d30cb709ad93
shtakai <shtakai@gmail.com>
2016-08-14 15:25:29 +0900

    added test_user_rerationshop

--- test/test_user_model.py
+++ test/test_user_model.py
@@ -13,3 +13,36 @@ def test_create_user_instance(session):
     assert user.id is not None
     assert user.followed.count() == 0
     assert user.newsfeed().count() == 0
+
+
+def test_user_relationships(session):
+
+    user_1 = models.User(
+        email='test1@example.com',
+        username='test1',
+        password='foobarbaz'
+    )
+
+    user_2 = models.User(
+        email='test2@example.com',
+        username='test2',
+        password='bingbarboo'
+    )
+
+    session.add(user_1)
+    session.add(user_2)
+    session.commit()
+
+    assert user_1.followed.count() == 0
+    assert user_2.followed.count() == 0
+
+    user_1.follow(user_2)
+
+    assert user_1.is_following(user_2) is True
+    assert user_2.is_following(user_1) is False
+    assert user_1.followed.count() == 1
+
+    user_1.unfollow(user_2)
+
+    assert user_1.is_following(user_2) is False
+    assert user_1.followed.count() == 0
